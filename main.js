9d7904a288249c2505c2514ec49d2f6fcf9d9cca
pgiurda <pgiurda@hotmail.com>
2026-08-06 14:02:05 -0300

    Cuarta Entrega 03/08

--- main.js
+++ main.js
@@ -1,37 +1,83 @@
-let precioXKm = 2000;
-const precioXAyudante = 5000;
 
-let calculoViaje = function (kmDelViaje, precio) {
-    let viaje = kmDelViaje * precio;
-    return viaje;
-}
+const carritoDeCompras = ["Lavandina", "Jabon liquido", "Pascualina", "Carne molida", "Fideos"].sort();
+let continuar = confirm(("Bienvenido al supermecado JS\n Ya tienes tu orden habitual lista.\n Necesita algo mas de la tienda??"))
 
-function isFueraDeCordoba(kmDelViaje){
-    if(kmDelViaje > 400){
-        precioXKm = 3000;
-        console.log("El viaje es fuera de Cordoba, el precio por kilometro es de: " + precioXKm);
+const agregarProductoAlFinal = function(){
+    const productoAAgregar = prompt("Que producto desea agregar a la lista? ")
+    carritoDeCompras.push(productoAAgregar)
+    carritoActual(carritoDeCompras)} ;
+
+const agregarProductoAlPrincipio = function(){
+    const productoAAgregar = prompt("Que producto esencial quieres agregar? ")
+    carritoDeCompras.unshift(productoAAgregar)
+    carritoActual(carritoDeCompras)};
+
+function posicionProducto(producto, posicion){
+    if(posicion < 2){
+    alert("El producto " + producto + " es esencial y se encuentra en la posición: " + posicion); 
+    }
+    else{
+    alert("El producto " + producto + " no es necesario y se encuentra en la posición: " + posicion); 
     }
 }
-function cantidadDeAyudantes(cantidad, precio){
-    return cantidad * precio;
-}
 
-if(confirm("Buenos dias, usted necesita un flete?")) {
-    let km = Number(prompt("Ingrese la cantidad de kilometros del viaje."))
-    isFueraDeCordoba(km);
-    let precioDelViaje = calculoViaje(km, precioXKm);
-    console.log("El precio del viaje es de: " + precioDelViaje);
-
-    let ayudante = confirm("Desea agregar un ayudante al viaje? (El costo es de $5000)");
-
-    if(ayudante){
-        let cantAyudantes = Number(prompt("Ingrese la cantidad de ayudantes que desea agregar al viaje."));
-        let precioConAyudante = (viajeConAyudante) => ayudante ? precioDelViaje + cantidadDeAyudantes(cantAyudantes, precioXAyudante) : precioDelViaje;
-        console.log("Usted contrato " + cantAyudantes + " ayudante(s) para el viaje.");
-        console.log("El precio final del viaje con ayudantes de: " + precioConAyudante(precioDelViaje))
+function buscarProducto(productoABuscar){
+    if(carritoDeCompras.includes(productoABuscar)){
+        alert("El producto " + productoABuscar + " se encuentra en el carrito");    
+    } else {
+        alert("El producto " + productoABuscar + " no se encuentra en el carrito");
+        if(confirm("Desea agregarlo? ")){
+            carritoDeCompras.splice(2, 0, productoABuscar)
+            alert("Se agrego correctamente")
+            carritoActual(carritoDeCompras);
         }else{
-            console.log("Usted no contrato ayudantes para el viaje.");
-            console.log("El precio final del viaje es de: " + precioDelViaje);
+            alert("No se agrego al carrito")
+            carritoActual(carritoDeCompras)}
     }
 }
-alert("Gracias por utilizar nuestro servicio de fletes, vuelva pronto!");
+
+const carritoActual = (carrito) => {
+    for(producto of carrito){
+        console.log("Producto: " + producto)
+    }
+};
+
+
+while(continuar){   
+        const opcion = prompt("Seleccione una opción:\n1. Agregar producto al final\n2. Agregar producto al principio\n3. Eliminar producto\n4. Buscar producto en la lista\n5. Posicion del producto\n0. Salir")
+        
+        switch(opcion) {
+        case "1":
+            agregarProductoAlFinal()
+            break
+        case "2":
+            agregarProductoAlPrincipio()
+            break
+        case "3":
+            if(confirm("Quiere eliminar el ultimo producto?")){
+            const productoEliminado = carritoDeCompras.pop()
+            console.log("El producto eliminado del carrito es " + productoEliminado)
+            }
+            break
+        case "4":
+            const productoBuscado = prompt("Que producto desea buscar en la lista??")
+            buscarProducto(productoBuscado)
+            break
+        case "5":
+            const productoIncluido = prompt("Ingrese el producto que desea buscar: ");
+            const indexProducto = carritoDeCompras.indexOf(productoIncluido);
+            posicionProducto(productoIncluido, indexProducto)
+            break
+        case "0":
+            continuar = false
+            alert("Gracias por comprar con nosotros")
+            break
+        default:
+            alert("Opcion Invalida")
+            break
+        }
+    }
+console.log("Tu lista de compra final es: ")
+carritoActual(carritoDeCompras)
+
+
