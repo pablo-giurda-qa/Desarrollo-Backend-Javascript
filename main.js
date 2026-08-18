c4fa4b97ef985167ac5a988aec8bea7f9ef15d82
pgiurda <pgiurda@hotmail.com>
2026-08-18 00:48:23 -0300

    QUinta c

--- main.js
+++ main.js
@@ -1,83 +1,55 @@
 
-const carritoDeCompras = ["Lavandina", "Jabon liquido", "Pascualina", "Carne molida", "Fideos"].sort();
-let continuar = confirm(("Bienvenido al supermecado JS\n Ya tienes tu orden habitual lista.\n Necesita algo mas de la tienda??"))
-
-const agregarProductoAlFinal = function(){
-    const productoAAgregar = prompt("Que producto desea agregar a la lista? ")
-    carritoDeCompras.push(productoAAgregar)
-    carritoActual(carritoDeCompras)} ;
-
-const agregarProductoAlPrincipio = function(){
-    const productoAAgregar = prompt("Que producto esencial quieres agregar? ")
-    carritoDeCompras.unshift(productoAAgregar)
-    carritoActual(carritoDeCompras)};
-
-function posicionProducto(producto, posicion){
-    if(posicion < 2){
-    alert("El producto " + producto + " es esencial y se encuentra en la posición: " + posicion); 
+class Vehiculo{
+    constructor(marca, modelo, precio, stock, venta){
+        this.marca = marca
+        this.modelo = modelo
+        this.precio = precio
+        this.stock = stock
+        this.venta = venta
     }
-    else{
-    alert("El producto " + producto + " no es necesario y se encuentra en la posición: " + posicion); 
+    cuotas(){
+        const cuotaAuto = Math.floor(this.precio/84)
+        console.log(`El plan de 84 cuotas es de ${cuotaAuto} pesos`)
     }
-}
-
-function buscarProducto(productoABuscar){
-    if(carritoDeCompras.includes(productoABuscar)){
-        alert("El producto " + productoABuscar + " se encuentra en el carrito");    
-    } else {
-        alert("El producto " + productoABuscar + " no se encuentra en el carrito");
-        if(confirm("Desea agregarlo? ")){
-            carritoDeCompras.splice(2, 0, productoABuscar)
-            alert("Se agrego correctamente")
-            carritoActual(carritoDeCompras);
-        }else{
-            alert("No se agrego al carrito")
-            carritoActual(carritoDeCompras)}
-    }
-}
-
-const carritoActual = (carrito) => {
-    for(producto of carrito){
-        console.log("Producto: " + producto)
-    }
-};
-
-
-while(continuar){   
-        const opcion = prompt("Seleccione una opción:\n1. Agregar producto al final\n2. Agregar producto al principio\n3. Eliminar producto\n4. Buscar producto en la lista\n5. Posicion del producto\n0. Salir")
-        
-        switch(opcion) {
-        case "1":
-            agregarProductoAlFinal()
-            break
-        case "2":
-            agregarProductoAlPrincipio()
-            break
-        case "3":
-            if(confirm("Quiere eliminar el ultimo producto?")){
-            const productoEliminado = carritoDeCompras.pop()
-            console.log("El producto eliminado del carrito es " + productoEliminado)
-            }
-            break
-        case "4":
-            const productoBuscado = prompt("Que producto desea buscar en la lista??")
-            buscarProducto(productoBuscado)
-            break
-        case "5":
-            const productoIncluido = prompt("Ingrese el producto que desea buscar: ");
-            const indexProducto = carritoDeCompras.indexOf(productoIncluido);
-            posicionProducto(productoIncluido, indexProducto)
-            break
-        case "0":
-            continuar = false
-            alert("Gracias por comprar con nosotros")
-            break
-        default:
-            alert("Opcion Invalida")
-            break
+    actualizarStock(){
+        if(this.venta){
+            this.stock -= 1
         }
+        return this.stock
     }
-console.log("Tu lista de compra final es: ")
-carritoActual(carritoDeCompras)
+}
 
+const vehiculo1 = new Vehiculo("Chevrolet", "Onix" , 15000000, 20, true);
+const vehiculo2 = new Vehiculo("Ford", "Fiesta", 18000000 , 26, false)
+const vehiculo3 = new Vehiculo("Peugeot", "208", 20000000, 30, true)
 
+console.log("Bienvenido a la concesionaria JS")
+
+// Primero mostraba por console.log cada propiedad del objeto 
+
+// console.log(`LLevate tu ${vehiculo1.marca} ${vehiculo1.modelo} por un precio total de ${vehiculo1.precio}`)
+// vehiculo1.cuotas()
+// console.log(`El stock actual es de ${vehiculo1.actualizarStock()} autos del ${vehiculo1.marca}`)
+
+// console.log(`LLevate tu ${vehiculo2.marca} ${vehiculo2.modelo} por un precio total de ${vehiculo2.precio}`)
+// vehiculo2.cuotas()
+// console.log(`El stock actual es de ${vehiculo2.actualizarStock()} autos del ${vehiculo2.marca}`)
+
+// console.log(`LLevate tu ${vehiculo3.marca} ${vehiculo3.modelo} por un precio total de ${vehiculo3.precio}`)
+// vehiculo3.cuotas()
+// console.log(`El stock actual es de ${vehiculo3.actualizarStock()} autos del ${vehiculo3.marca}`)
+
+//Luego preferi almacenar los objetos en variables
+const arrayVehiculos = [vehiculo1, vehiculo2, vehiculo3];
+
+// Al final, crear una funcion que itere en ese array de objetos y me muestre la informacion de cada objeto asi no repetia codigo
+
+function mostrarInfo(vehiculos) {
+    for (const vehiculo of vehiculos) {
+        console.log(`Llevate tu ${vehiculo.marca} ${vehiculo.modelo} por un precio total de ${vehiculo.precio}` );
+        vehiculo.cuotas();
+        console.log(`El stock actual es de ${vehiculo.actualizarStock()} autos de ${vehiculo.marca}`);
+    }
+}
+
+mostrarInfo(arrayVehiculos)
